669cf3ea9474967c0a9510674fe4e0df27a6e17a
MasterGeniusBananaGuy <89812974+MasterGeniusBananaGuy@users.noreply.github.com>
2021-12-18 22:58:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,41 @@ from radius_and_diameter import rad_and_dia as radia
 from mass_density_volume_and_surface_area import mass_dens_vol_and_surf as mdvs
 import os
 import time
+import json
+
+
+
+def stop():
+	quit()
+
+
+
+def start():
+	os.system('clear')
+	load = input("Would you like to load a previus planet? (yes/no) ")
+	if load == 'yes':
+		name = input("what planet would you like to load: ")
+		with open("saves.json", "r") as file:
+			saves = json.load(file)
+			planets = saves["planets"]
+
+			if name in planets:
+				planet = planets[f"{name}"]
+				r = planet["Radius"]
+				d = planet["Diameter"]
+				plcic = planet["Planet Circumference"]
+				vol = planet["Volume"]
+				mass = planet["Mass"]
+				surf = planet["Surface Area"]
+			else:
+				print("Planet not saved or misspelled!")
+				time.sleep(5)
+				stop()
+			print(r, d, plcic, vol, mass, surf)
+	else:
+		main()
+
+
 
 def main():
 	os.system('clear')
@@ -18,14 +53,20 @@ def main():
 
 	print(r, d, plcic, vol, mass, surf)
 	time.sleep(5)
-	stop(r, d, plcic, vol, mass, surf)
-	
-def stop(r, d, plcic, vol, mass, surf):
+
 	os.system('clear')
-	save = input("Would you like to save this planet? (yes/no) ")
-	if save == 'yes':
-		planet = input("Name of the planet: ")
-		with open('saves.txt', 'a') as f:
+	save = input("Would you like to safe? (yes/no) ")
+	if save == "yes":
+		save(r, d, plcic, vol, mass, surf)
+	else:
+		stop()
+
+
+
+def save(r, d, plcic, vol, mass, surf):
+	os.system('clear')
+	planet = input("Name of the planet: ")
+	with open('saves.txt', 'a') as f:
 			f.write("Planet: " + str(planet) + "\n")
 			f.write("Radius: " + str(r) + " km " + "\n")
 			f.write("Diameter: " + str(d) + " km" + "\n")
@@ -33,32 +74,8 @@ def stop(r, d, plcic, vol, mass, surf):
 			f.write("Mass: " + str(mass) + "ton(s)" + "\n")
 			f.write("Volume: " + str(vol) + " km3" + "\n")
 			f.write("Surface Area: " + str(surf) + " km2" + "\n")
-		os.system('clear')
-		quit()
-	else:
-		os.system('clear')
-		quit()
-	
-def start():
-	os.system('clear')
-	load = input("Would you like to load a previus planet? (yes/no) ")
-	if load == 'yes':
-		with open('saves.txt', 'r') as f:
-			lines = [line.strip() for line in f]
-			for line in lines:
-				planet = lines[0] + "\n"
-				r = lines[1] + " km" + "\n"
-				d = lines[2] + " km" + "\n"
-				plcic = lines[3] + " km" + "\n"
-				mass = lines[4] + " ton(s)" + "\n"
-				vol = lines[5] + " km3" + "\n"
-				surf = lines[6] + " km2" + "\n"
-			print(planet + r + d + plcic + mass)
-			time.sleep(5)
-			os.system('clear')
-			quit()
-	else:
-		main()
+	stop()
+
 
 
 start()
